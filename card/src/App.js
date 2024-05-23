a33435c90c34bb5c258f20405322b242a91a3fde
Harshini-priyaa <harshinipriyaa1112@gmail.com>
2024-05-23 13:10:42 +0530

    CardwithForm

---
+++ card/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CardWithForm } from './components/ui/CardWithForm'; 
+import './App.css';
+const App = () => {
+  return (
+    <div className="App">
+      <CardWithForm />
+    </div>
+  );
+};
+
+export default App;
